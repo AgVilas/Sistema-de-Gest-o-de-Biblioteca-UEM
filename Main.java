4c32a55db43ad24920cd0b731c2cc325fc7d20f6
Agvilas <Agvilas.Agnelo.vilanculo@outlook.pt>
2025-06-18 17:40:04 +0200

    Forgot the commit main

---
+++ Main.java
@@ -0,0 +1,9 @@
+import Service.MenuUI;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        MenuUI menu = new MenuUI(); // Agora MenuUI
+        menu.menuPrincipal();
+    }
+}
